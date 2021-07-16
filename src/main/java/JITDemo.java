e2e83bcdeec04f5a1f594a676c4903ba4c9046ca
youliang <youliang.feng@zoom.us>
2021-07-16 02:12:59 +0000

    add comment

--- src/main/java/JITDemo.java
+++ src/main/java/JITDemo.java
@@ -1,5 +1,18 @@
 /**
  * @author Youliang Feng
+ *
+ * -Xint：参数强制虚拟机处于"解释模式"此时编译器不工作
+ * -Xcomp：参数强制虚拟机处于"编译模式"此时解释器不工作
+ * -server：参数使C1不工作
+ * -client：参数使C2不工作
+ * -XX:+PrintCompilation 来打印即时编译情况
+ *
+ * The compiler in the server VM now provides correct stack backtraces for all "cold" built-in exceptions.
+ * For performance purposes, when such an exception is thrown a few times, the method may be recompiled.
+ * After recompilation, the compiler may choose a faster tactic using preallocated exceptions that do not provide a stack trace.
+ * To disable completely the use of preallocated exceptions, use this new flag: -XX:-OmitStackTraceInFastThrow.
+ *
+ * https://juejin.cn/post/6844904166943358989
  */
 public class JITDemo {
 
@@ -15,7 +28,7 @@ public class JITDemo {
             // 人工构造异常抛出的场景
             ((Object) null).getClass();
         } catch (Exception e) {
-            if (e.getStackTrace().length == 0) {
+            if (e.getStackTrace().length == 0) {  // 即时编译导致堆栈丢失
                 try {
                     // 堆栈消失的时候当前线程休眠5秒，便于观察
                     Thread.sleep(5000);
